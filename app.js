d33e1f55fc68fc14dbba696ad9fe130c851483d3
tanyaweaver <tanya83@uw.edu>
2016-04-13 09:06:51 -0700

    added 4 questions

--- app.js
+++ app.js
@@ -1,21 +1,66 @@
 var visitorName = prompt('Hi! Welcome to my page! What is your name?');
 console.log('Visitor Name is ' + visitorName);
 
-var visitorWantsToPlay = prompt(visitorName + ', do you want to play a guessing game about me? (Y/N)').toUpperCase();
-console.log('Did ' + visitorName + ' want to play the guessing game? ' + visitorWantsToPlay);
+var visitorWantsToPlay = alert(visitorName + ', let\'s play a guessing game about me!');
 
-if (visitorWantsToPlay === 'Y' || visitorWantsToPlay === 'YES')
+var question1 = prompt('My goal is to become a software developer in python (Y/N)').toUpperCase();
+console.log(visitorName + '\'s answer to the question was ' + question1);
+
+if (question1 === 'Y' || question1 === 'YES')
 {
-  var question1 = prompt('My goal is to become a software developer in python (Y/N)').toUpperCase();
-  console.log(visitorName + '\'s answer to the question was ' + question1);
-
-  if (question1 === 'Y' || question1 === 'YES')
-  {
-    alert(visitorName + ', you got it right!');
-  }
-  else
-  {
-    alert(visitorName + ', you got it wrong :(');
-  }
+  alert(visitorName + ', you got it right!');
 }
+else
+{
+  alert(visitorName + ', you got it wrong :(');
+}
+
+var question2 = prompt('I am from Russia (Y/N)').toUpperCase();
+console.log(visitorName + '\'s answer to the question was ' + question2);
+
+if (question2 === 'Y' || question2 === 'YES')
+{
+  alert(visitorName + ', you got it right!');
+}
+else
+{
+  alert(visitorName + ', you got it wrong :(');
+}
+
+var question3 = prompt('I moved to the US in 2006 (Y/N)').toUpperCase();
+console.log(visitorName + '\'s answer to the question was ' + question3);
+
+if (question3 === 'Y' || question3 === 'YES')
+{
+  alert(visitorName + ', you got it right!');
+}
+else
+{
+  alert(visitorName + ', you got it wrong :(');
+}
+
+var question4 = prompt('I studied Industrial Engineering in Russia').toUpperCase();
+console.log(visitorName + '\'s answer to the question was ' + question4);
+
+if (question4 === 'Y' || question4 === 'YES')
+{
+  alert(visitorName + ', you got it right!');
+}
+else
+{
+  alert(visitorName + ', you got it wrong :(');
+}
+
+var question5 = prompt('I majored in Medical Lab Science (Y/N)').toUpperCase();
+console.log(visitorName + '\'s answer to the question was ' + question5);
+
+if (question5 === 'Y' || question5 === 'YES')
+{
+  alert(visitorName + ', you got it right!');
+}
+else
+{
+  alert(visitorName + ', you got it wrong :(');
+}
+
 alert('See you later, ' + visitorName);
