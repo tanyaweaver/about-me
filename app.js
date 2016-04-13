767c11d93d7c9c96745db68731d99d6c6a8f9bcb
tanyaweaver <tanya83@uw.edu>
2016-04-13 07:27:24 -0700

    changed the if logic

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 var visitorName = prompt('Hi! Welcome to my page! What is your name?');
 console.log('Visitor Name is ' + visitorName);
 
-var visitorWantsToPlay = prompt(visitorName + ', do you want to play a quessing game about me? (Y/N)').toUpperCase();
+var visitorWantsToPlay = prompt(visitorName + ', do you want to play a guessing game about me? (Y/N)').toUpperCase();
 console.log('Did ' + visitorName + ' want to play the guessing game? ' + visitorWantsToPlay);
 
-if (visitorWantsToPlay === 'N' || visitorWantsToPlay === 'No')
+if (visitorWantsToPlay === 'Y' || visitorWantsToPlay === 'YES')
 {
-  alert('Ok, ' + visitorName + ', whatever :/. We will play it anyway!');
-};
+  //alert('Ok, ' + visitorName + ', whatever :/. We will play it anyway!');
+//};
 
-var question1 = prompt('My goal is to become a software developer in python (Y/N)').toUpperCase();
-console.log('Did ' + visitorName + ' get the answer right? ' + question1);
+  var question1 = prompt('My goal is to become a software developer in python (Y/N)').toUpperCase();
+  console.log(visitorName + '\s answer to the question was ' + question1);
 
-if (question1 === 'Y' || question1 === 'YES')
-{
-  alert(visitorName + ', you got it right! See you later!');
+  if (question1 === 'Y' || question1 === 'YES')
+  {
+    alert(visitorName + ', you got it right!');
+  }
+  else
+  {
+    alert(visitorName + ', you got it wrong :(');
+  }
 }
-else
- {
-  alert(visitorName + ', you got it wrong :( See you later!');
-};
+alert('See you later, ' + visitorName);
